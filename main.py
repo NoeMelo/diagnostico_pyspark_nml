5e6c551239a23310ac716f8486135c51697caa23
Noe Melo Locumber <15101369@ue.edu.pe>
2021-09-08 13:07:43 -0500

    se quito codigo que no sirve en el main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from pyspark.sql import SparkSession
 
 from minsait.ttaa.datio.common.Constants import *
 from minsait.ttaa.datio.engine.Transformer import Transformer
-import os
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 if __name__ == '__main__':
     spark: SparkSession = SparkSession \
